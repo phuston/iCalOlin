c7d330dcf1bd8a6a64b2fead1dc03b6bbd7550b6
Patrick Huston <patrickhuston1@gmail.com>
2014-12-18 21:10:48 -0500

    Merging frontend and backend

--- app/views.py
+++ app/views.py
@@ -6,6 +6,16 @@ from flask import render_template, flash, redirect, session, url_for, request, g
 def index():
 	return render_template('index.html')
 
+# @app.route('/', methods=['POST'])
+# def index():
+
+#     username = request.form['text']
+#     password = request.form['password']
+#     email = request.form['emailAddress']
+    
+#     #Here is where the iCal should go now.
+
+
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
