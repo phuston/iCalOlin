913020a894bba674f141fbacd38f3435c6c6129a
Patrick Huston <patrickhuston1@gmail.com>
2015-02-17 17:36:37 -0500

    Updates 404 error handling

--- app/views.py
+++ app/views.py
@@ -57,8 +57,3 @@ def index():
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_error(error):
-    db.session.rollback()
-    return render_template('500.html'), 500
